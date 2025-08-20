de1b93236d949a460f30502fd983625cbc27a09d
yofanalfiatur <60646990+yofanalfiatur@users.noreply.github.com>
2025-08-20 15:32:49 +0700

    add : app.js & next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,19 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "https://secom.madebystucel.com",
+    "https://secom-fe.vercel.app/",
+  ],
+  experimental: {
+    workerThreads: false,
+    cpus: 2,
+  },
+};
 
 export default withNextIntl(nextConfig);
