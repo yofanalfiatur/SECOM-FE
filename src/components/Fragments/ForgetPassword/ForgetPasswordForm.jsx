d82420ab64020395c4fc9a5f1f1ab5411168a2dc
yofanalfiatur <60646990+yofanalfiatur@users.noreply.github.com>
2025-09-03 15:49:10 +0700

    update : gitlab text field forgot password

--- src/components/Fragments/ForgetPassword/ForgetPasswordForm.jsx
+++ src/components/Fragments/ForgetPassword/ForgetPasswordForm.jsx
@@ -78,9 +78,12 @@ const ForgetPasswordForm = () => {
             transition={{ duration: 0.5, delay: 0.8 }}
             className="relative overflow-hidden flex flex-col mb-2 lg:mb-6 w-full"
           >
+            <p className="text-white font-medium text-[12px] lg:text-base mt-2 lg:mb-2 ml-[2px] tracking-[1px]">
+              {locale === "en" ? "Forgot Password?" : "Lupa Password?"}
+            </p>
             <div className="flex flex-row">
               <div
-                className={`relative flex flex-col rounded-[5px] overflow-hidden w-[80%] form__wrap__input ${
+                className={`relative flex flex-col rounded-[5px] overflow-hidden w-[68%] lg:w-[80%] form__wrap__input ${
                   errors.user ? "border-red-500" : ""
                 }`}
               >
@@ -105,7 +108,7 @@ const ForgetPasswordForm = () => {
               </div>
               <button
                 onClick={() => handleSubmit("user")} // DITAMBAHKAN: Handler untuk tombol
-                className="bg-tosca w-[20%] cursor-pointer m-[3px] text-white text-sm lg:text-xl px-4 py-3 lg:px-5 lg:py-3.5 rounded-[5px] tracking-[3px] leading-none uppercase hover:bg-teal-600 transition-colors"
+                className="bg-tosca w-[30%] lg:w-[20%] cursor-pointer m-[3px] text-white text-sm lg:text-xl px-4 py-3 lg:px-5 lg:py-3.5 rounded-[5px] tracking-[3px] leading-none uppercase hover:bg-teal-600 transition-colors"
               >
                 {locale === "en" ? "SEND" : "KIRIM"}
               </button>
@@ -129,9 +132,12 @@ const ForgetPasswordForm = () => {
             transition={{ duration: 0.5, delay: 0.9 }}
             className="relative overflow-hidden flex flex-col mb-2 lg:mb-6 w-full"
           >
+            <p className="text-white font-medium text-[12px] lg:text-base mt-2 lg:mb-2 ml-[2px] tracking-[1px]">
+              {locale === "en" ? "Forgot Username?" : "Lupa Username?"}
+            </p>
             <div className="flex flex-row">
               <div
-                className={`relative flex flex-col rounded-[5px] overflow-hidden w-[80%] form__wrap__input ${
+                className={`relative flex flex-col rounded-[5px] overflow-hidden w-[68%] lg:w-[80%] form__wrap__input ${
                   errors.email ? "border-red-500" : ""
                 }`}
               >
@@ -156,7 +162,7 @@ const ForgetPasswordForm = () => {
               </div>
               <button
                 onClick={() => handleSubmit("email")} // DITAMBAHKAN: Handler untuk tombol
-                className="bg-tosca w-[20%] cursor-pointer m-[3px] text-white text-sm lg:text-xl px-4 py-3 lg:px-5 lg:py-3.5 rounded-[5px] tracking-[3px] leading-none uppercase hover:bg-teal-600 transition-colors"
+                className="bg-tosca w-[30%] lg:w-[20%] cursor-pointer m-[3px] text-white text-sm lg:text-xl px-4 py-3 lg:px-5 lg:py-3.5 rounded-[5px] tracking-[3px] leading-none uppercase hover:bg-teal-600 transition-colors"
               >
                 {locale === "en" ? "SEND" : "KIRIM"}
               </button>
