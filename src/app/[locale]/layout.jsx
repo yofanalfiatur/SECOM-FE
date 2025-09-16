dde0c2e0fd06ebb0ae5a59e781d621ff6849e5bd
yofanalfiatur <60646990+yofanalfiatur@users.noreply.github.com>
2025-09-16 17:15:20 +0700

    update : GTM client

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -5,6 +5,7 @@ import "../globals.css";
 import Header from "@/components/Layouts/Header";
 import Footer from "@/components/Layouts/Footer";
 import ProgressBar from "@/components/Layouts/ProgressBar";
+import Script from "next/script";
 
 export default async function LocaleLayout({ children, params }) {
   const { locale } = await params;
@@ -23,8 +24,29 @@ export default async function LocaleLayout({ children, params }) {
         <title>SECOM</title>
         <meta name="description" content="Website for SECOM" />
         <meta name="robots" content="index, follow" />
+
+        {/* Google Tag Manager (head) */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MZZM55X');
+          `}
+        </Script>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MZZM55X"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <NextIntlClientProvider locale={locale} messages={messages}>
           <ProgressBar />
           <Header />
